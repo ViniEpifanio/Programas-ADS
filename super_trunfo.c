d045b5677d642583f2c46c59441a5ef64b119bda
Vinícius Epifanio <viniciusgomesepifanio@gmail.com>
2025-09-01 21:18:14 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -4,18 +4,18 @@ int main(){
   char estado[50];
   char codigo[50];
   char nome[50];
-  int populacao, pontos, pib;
-  float area;
+  int populacao, pontos;
+  float area, pib;
 
   //Visualização na tela e leitura de informações//
   printf("Preencha o Estado (apenas uma palavra): ");
-  scanf("%s", &estado);
+  scanf("%s", estado);
 
   prinf("Preencha o código da carta com uma letra(A-H) seguido de número um (01-04): ");
-  scanf("%s", &codigo);
+  scanf("%s", codigo);
 
   printf("Preencha o nome da cidade (apenas uma palavra): ");
-  scanf("%s", &nome);
+  scanf("%s", nome);
 
   printf("Preencha a população da cidade: ");
   scanf("%d", &populacao);
@@ -24,7 +24,7 @@ int main(){
   scanf("%f", &area);
 
   printf("Preencha o PIB da cidade: ");
-  scanf("%d", &pib);
+  scanf("%f", &pib);
 
   printf("Prencha o número de pontos turísticos da cidade: ");
   scanf("%d", &pontos);
@@ -33,7 +33,7 @@ int main(){
 
   printf("Carta 1\n");
   printf(" Estado: %s\n Código da carta: %s\n Nome da cidade: %s\n", estado, codigo, nome);
-  printf(" População da cidade: %d\n Área da cidade: %f KM²\n PIB da cidade: %d\n Número de pontos turísticos: %d\n", populacao, area, pib, pontos);
+  printf(" População da cidade: %d\n Área da cidade: %f KM²\n PIB da cidade: %f\n Número de pontos turísticos: %d\n", populacao, area, pib, pontos);
 
   return 0;
   
